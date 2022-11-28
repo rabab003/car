a9123c76db1b47f6c942e56ff84f3473cc0835c5
Rabab  Qasim <94569281+rabab003@users.noreply.github.com>
2022-11-28 19:53:31 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let imgggg = document.querySelector('.img');
+let colorsssss = document.querySelector('.container')
+
+function phones(phone) {
+    imgggg.src = phone;
+}
+
+function colors(color) {
+    colorsssss.style.background = color;
+}
